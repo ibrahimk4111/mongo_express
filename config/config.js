4c7ca1b84fad3dcb66d2d3462170d126bda77bbe
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-12-26 21:15:18 +0600

    fetch url fixs + local host

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
         port: process.env.PORT || 8800    
     },
     url:{
-        dburl: process.env.DB_URL || "mongodb://localhost:27017/backend_express_practice"
+        dburl: process.env.DB_URL || "mongodb://127.0.0.1:27017/backend_express_practice"
     }
 }
 
