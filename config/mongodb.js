50a60332925f26426e68ccce66b9d2b2049af4d7
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-12-27 00:30:10 +0600

    url

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,20 +3,8 @@ const config = require("./config");
 
 const uri = config.url.dburl;
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-  autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-}
-
-
 mongoose
-  .connect(uri, options)
+  .connect(uri)
   .then(() => {
     console.log("Mongoose is connected");
   })
